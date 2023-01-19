812ae69c508f7e18f30071825583c2bfa240fac7
LilianaGallego <lilianagallegom@gmail.com>
2023-01-19 09:24:27 -0500

    new: Se actualiza la entidad Manipulador con su objeto de valor Seccion y su comportamiento

--- Dominio/src/main/java/org/sofka/personal/entidades/Manipulador.java
+++ Dominio/src/main/java/org/sofka/personal/entidades/Manipulador.java
@@ -1,4 +1,22 @@
 package org.sofka.personal.entidades;
+import co.com.sofka.domain.generic.Entity;
+import org.sofka.personal.values.Seccion;
+import org.sofka.personal.values.ManipuladorId;
+public class Manipulador extends Entity<ManipuladorId> {
 
-public class Manipulador {
+    private Seccion seccion ;
+
+     public Manipulador(ManipuladorId manipuladorId, Seccion seccion) {
+        super(manipuladorId);
+         this.seccion = seccion;
+    }
+
+    ///FALTA verificar
+    public void agregarManipulador(ManipuladorId manipuladorId, Seccion seccion){
+
+    }
+
+    public Seccion seccion(){
+        return seccion;
+    }
 }
