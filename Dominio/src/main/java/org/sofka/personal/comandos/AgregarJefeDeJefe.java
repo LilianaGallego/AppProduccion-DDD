df89d7cf24b392e87f56aa984a7c1cf6dae6bff2
LilianaGallego <lilianagallegom@gmail.com>
2023-01-19 09:20:20 -0500

    new: Se crea el comando para el comportamiento AgregarJefeDeJefe

--- Dominio/src/main/java/org/sofka/personal/comandos/AgregarJefeDeJefe.java
+++ Dominio/src/main/java/org/sofka/personal/comandos/AgregarJefeDeJefe.java
@@ -1,4 +1,24 @@
 package org.sofka.personal.comandos;
 
-public class AgregarJefeDeJefe {
+import co.com.sofka.domain.generic.Command;
+import org.sofka.personal.values.Experiencia;
+import org.sofka.personal.values.JefeId;
+
+public class AgregarJefeDeJefe extends Command {
+    private final JefeId jefeId;
+    private final Experiencia experiencia;
+
+    public AgregarJefeDeJefe(JefeId jefeId, Experiencia experiencia) {
+        this.jefeId = jefeId;
+        this.experiencia = experiencia;
+    }
+
+
+    public JefeId getJefeId() {
+        return jefeId;
+    }
+
+    public Experiencia getExperiencia() {
+        return experiencia;
+    }
 }
