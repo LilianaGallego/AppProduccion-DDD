a9d460b78ff036c8590fe827459a207f3ade9dc6
LilianaGallego <lilianagallegom@gmail.com>
2023-01-19 16:59:54 -0500

    new:Se actulizan los objetos de valor con algunas validaciones

--- Dominio/src/main/java/org/sofka/personal/values/ManipuladorId.java
+++ Dominio/src/main/java/org/sofka/personal/values/ManipuladorId.java
@@ -3,4 +3,14 @@ package org.sofka.personal.values;
 import co.com.sofka.domain.generic.Identity;
 
 public class ManipuladorId extends Identity {
+    public ManipuladorId(){
+
+    }
+    private ManipuladorId(String id){
+        super(id);
+    }
+    public static ManipuladorId of(String id){
+        return new ManipuladorId(id);
+
+    }
 }
--- Dominio/src/main/java/org/sofka/personal/values/PersonalId.java
+++ Dominio/src/main/java/org/sofka/personal/values/PersonalId.java
@@ -3,4 +3,14 @@ package org.sofka.personal.values;
 import co.com.sofka.domain.generic.Identity;
 
 public class PersonalId extends Identity {
+    public PersonalId(){
+
+    }
+    private PersonalId(String id){
+        super(id);
+    }
+    public static PersonalId of(String id){
+        return new PersonalId(id);
+
+    }
 }
