0249f1faf63b21ff274b0ed383be0c3e3547d885
LilianaGallego <lilianagallegom@gmail.com>
2023-01-19 09:20:57 -0500

    new: Se crea el comando para el comportamiento AgregarManipuladorDeManipulador

--- Dominio/src/main/java/org/sofka/personal/comandos/AgregarManipuladorDeManipulador.java
+++ Dominio/src/main/java/org/sofka/personal/comandos/AgregarManipuladorDeManipulador.java
@@ -1,4 +1,23 @@
 package org.sofka.personal.comandos;
 
-public class AgregarManipuladorDeManipulador {
+import co.com.sofka.domain.generic.Command;
+import org.sofka.personal.values.ManipuladorId;
+import org.sofka.personal.values.Seccion;
+
+public class AgregarManipuladorDeManipulador extends Command {
+    private final ManipuladorId manipuladorId;
+    private final Seccion seccion;
+
+    public AgregarManipuladorDeManipulador(ManipuladorId manipuladorId, Seccion seccion) {
+        this.manipuladorId = manipuladorId;
+        this.seccion = seccion;
+    }
+
+    public ManipuladorId getManipuladorId() {
+        return manipuladorId;
+    }
+
+    public Seccion getSeccion() {
+        return seccion;
+    }
 }
