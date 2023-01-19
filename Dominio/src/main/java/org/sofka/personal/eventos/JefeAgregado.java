8aea1e158a5b4c0f180e355b18e8a7c102f04292
LilianaGallego <lilianagallegom@gmail.com>
2023-01-19 09:25:23 -0500

    new: Se actualiza el evento JefeAgregado

--- Dominio/src/main/java/org/sofka/personal/eventos/JefeAgregado.java
+++ Dominio/src/main/java/org/sofka/personal/eventos/JefeAgregado.java
@@ -1,4 +1,17 @@
 package org.sofka.personal.eventos;
 
-public class JefeAgregado {
+import co.com.sofka.domain.generic.DomainEvent;
+import org.sofka.personal.values.Experiencia;
+import org.sofka.personal.values.JefeId;
+
+public class JefeAgregado extends DomainEvent {
+    private final JefeId jefeId;
+    private final Experiencia experiencia;
+
+
+    public JefeAgregado(JefeId jefeId, Experiencia experiencia) {
+        super("org.sofka.personal.eventos.jefeagregado");
+        this.jefeId = jefeId;
+        this.experiencia = experiencia;
+    }
 }
