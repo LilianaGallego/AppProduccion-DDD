deaa2205a3f4b605cf58e63ecb769eb8c6790425
LilianaGallego <lilianagallegom@gmail.com>
2023-01-19 23:17:36 -0500

    new:Se crea el caso de uso y el test para el comportamiento CrearInsumo

---
+++ Negocio/src/main/java/org/sofka/insumo/CrearInsumoUseCase.java
@@ -0,0 +1,17 @@
+package org.sofka.insumo;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import org.sofka.insumo.comandos.CrearInsumo;
+
+public class CrearInsumoUseCase extends UseCase<RequestCommand<CrearInsumo>, ResponseEvents> {
+    @Override
+    public void executeUseCase(RequestCommand<CrearInsumo> crearInsumoRequestCommand) {
+        var command = crearInsumoRequestCommand.getCommand();
+
+        var insumo = new Insumo(command.getInsumoId(), command.getClasificacion());
+
+        emit().onResponse(new ResponseEvents(insumo.getUncommittedChanges()));
+    }
+}
